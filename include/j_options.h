2e9d2309fb83b9acfc841792b58abbf6441573e3
Soner Çökmen <sonercokmen@gmail.com>
2026-02-11 12:08:04 +0100

    test: added coverage reporting workflow steps

--- include/j_options.h
+++ include/j_options.h
@@ -3,11 +3,11 @@
 
 #include <stdlib.h>
 
-#define BYTES_IN_KB    1024
-#define BYTES_IN_MB    (BYTES_IN_KB * BYTES_IN_KB)
-#define J_BUF_MIN_SIZE (1  * BYTES_IN_KB)
-#define J_BUF_DEF_SIZE (64 * BYTES_IN_KB)
-#define J_BUF_MAX_SIZE (64 * BYTES_IN_KB * BYTES_IN_KB)
+#define BYTES_IN_KB    1024      // 1KB
+#define BYTES_IN_MB    1048576   // 1MB
+#define J_BUF_MIN_SIZE 1024      // 1KB
+#define J_BUF_DEF_SIZE 65536     // 64KB
+#define J_BUF_MAX_SIZE 67108864  // 64MB
 #define J_QUE_MIN_SIZE 0
 #define J_QUE_DEF_SIZE 0
 #define J_QUE_MAX_SIZE 1024

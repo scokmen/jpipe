f662cd842ade3486c3c96351cbcd4446e741c1db
Soner Çökmen <sonercokmen@gmail.com>
2026-03-11 13:34:26 +0100

    ci: fixed ci/cd unknown target coverage error

--- cmake/quality.cmake
+++ cmake/quality.cmake
@@ -1,4 +1,4 @@
-function(__binary_ver BINARY_PATH FLAG VERSION)
+function(__get_binary_version BINARY_PATH FLAG VERSION)
     execute_process(
             COMMAND ${BINARY_PATH} ${FLAG}
             OUTPUT_VARIABLE COMMAND_OUT
@@ -9,11 +9,19 @@ endfunction()
 
 function(init_clang_tidy TARGET)
     if (NOT CMAKE_C_COMPILER_ID MATCHES "Clang")
-        message(WARNING "[clang-tidy]: disabled >> [cc=${CMAKE_C_COMPILER_ID}]")
+        message(WARNING "[clang-tidy]: skipped >> [cc=${CMAKE_C_COMPILER_ID}]")
         return()
     endif ()
+    
     find_program(CLANG_TIDY_BINARY NAMES "clang-tidy")
     if (CLANG_TIDY_BINARY)
+        __get_binary_version(
+                ${CLANG_TIDY_BINARY} 
+                "--version" 
+                CLANG_TIDY_VERSION
+        )
+        message(STATUS "[clang-tidy]: found >> version=v${CLANG_TIDY_VERSION}, path='${CLANG_TIDY_BINARY}'")
+        
         set(CLANG_TIDY_COMMAND
                 "${CLANG_TIDY_BINARY}"
                 "-config-file=${CMAKE_SOURCE_DIR}/.clang-tidy"
@@ -23,19 +31,26 @@ function(init_clang_tidy TARGET)
         set_target_properties(${TARGET} PROPERTIES
                 C_CLANG_TIDY "${CLANG_TIDY_COMMAND}"
         )
-        __binary_ver(${CLANG_TIDY_BINARY} "--version" CLANG_TIDY_VERSION)
-        message(STATUS "[clang-tidy]: enabled >> version=v${CLANG_TIDY_VERSION}, path='${CLANG_TIDY_BINARY}'")
+        message(STATUS "[clang-tidy]: activated")
     else ()
-        message(WARNING "[clang-tidy]: disabled >> [not found]")
+        message(WARNING "[clang-tidy]: skipped >> [not found]")
     endif ()
 endfunction()
 
 function(init_clang_format)
     find_program(CLANG_FORMAT_BINARY clang-format)
     if (CLANG_FORMAT_BINARY)
+        __get_binary_version(
+                ${CLANG_FORMAT_BINARY} 
+                "--version" 
+                CLANG_FORMAT_VERSION
+        )
+        message(STATUS "[clang-format]: found >> version=v${CLANG_FORMAT_VERSION}, path='${CLANG_FORMAT_BINARY}'")
+        
         file(GLOB_RECURSE ALL_SOURCE_FILES
                 "${CMAKE_SOURCE_DIR}/src/*.c"
                 "${CMAKE_SOURCE_DIR}/include/*.h")
+        
         if (NOT TARGET format)
             add_custom_target(format
                     COMMAND ${CLANG_FORMAT_BINARY}
@@ -47,38 +62,40 @@ function(init_clang_format)
                     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
                     COMMENT "[clang-format]: analysing..."
             )
+            message(STATUS "[clang-format]: targeted >> [target=format]")
         endif ()
-        __binary_ver(${CLANG_FORMAT_BINARY} "--version" CLANG_FORMAT_VERSION)
-        message(STATUS "[clang-format]: enabled >> version=v${CLANG_FORMAT_VERSION}, path='${CLANG_FORMAT_BINARY}'")
     else ()
-        message(WARNING "[clang-format]: disabled >> [not found]")
+        message(WARNING "[clang-format]: skipped >> [not found]")
     endif ()
 endfunction()
 
 function(init_coverage_flags TARGET)
     find_program(LCOV_BINARY NAMES "lcov")
     if (LCOV_BINARY)
-        __binary_ver(${LCOV_BINARY} "--version" LCOV_VERSION)
-        message(STATUS "[lcov]: enabled >> version=v${LCOV_VERSION}, path='${LCOV_BINARY}'")
+        __get_binary_version(
+                ${LCOV_BINARY} 
+                "--version" 
+                LCOV_VERSION
+        )
+        message(STATUS "[lcov]: found >> version=v${LCOV_VERSION}, path='${LCOV_BINARY}'")
     else ()
-        message(WARNING "[lcov]: disabled >> [not found]")
+        message(WARNING "[lcov]: skipped >> [not found]")
     endif ()
 
     find_program(GCOVR_BINARY NAMES "gcovr")
     if (GCOVR_BINARY)
-        execute_process(
-                COMMAND ${GCOVR_BINARY} --version
-                OUTPUT_VARIABLE GCOVR_VERSION
-                OUTPUT_STRIP_TRAILING_WHITESPACE
+        __get_binary_version(
+                ${GCOVR_BINARY} 
+                "--version" 
+                GCOVR_VERSION
         )
-        __binary_ver(${GCOVR_BINARY} "--version" GCOVR_VERSION)
-        message(STATUS "[gcovr]: enabled >> version=v${GCOVR_VERSION}, path='${GCOVR_BINARY}'")
+        message(STATUS "[gcovr]: found >> version=v${GCOVR_VERSION}, path='${GCOVR_BINARY}'")
     else ()
-        message(WARNING "[gcovr]: disabled >> [not found]")
+        message(WARNING "[gcovr]: skipped >> [not found]")
     endif ()
 
     if (NOT (LCOV_BINARY AND GCOVR_BINARY))
-        message(WARNING "[coverage]: disabled >> [lcov and gcovr must be available]")
+        message(WARNING "[coverage]: skipped >> [lcov || gcovr missing]")
         return()
     endif ()
 
@@ -99,6 +116,6 @@ function(init_coverage_flags TARGET)
                 WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                 COMMENT "[coverage]: analysing..."
         )
+        message(STATUS "[coverage]: targeted >> [target=coverage]")
     endif ()
-    message(STATUS "[coverage]: enabled")
 endfunction()

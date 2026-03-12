06098a6d6c3ec5a25533ab4c07b2712c432c90a1
Soner Çökmen <sonercokmen@gmail.com>
2026-03-12 19:48:04 +0100

    ci: fixed epoll compile error

--- src/poller/epoll.c
+++ src/poller/epoll.c
@@ -2,18 +2,17 @@
 #include <jp_errno.h>
 #include <jp_poller.h>
 
-jp_poller_t* jp_poller_create(JP_UNUSED int timeout) {
+jp_poller_t* jp_poller_create(JP_ATTR_UNUSED int timeout) {
     return NULL;
 }
 
-jp_errno_t jp_poller_poll(JP_UNUSED jp_poller_t* poller, JP_UNUSED int fd) {
+jp_errno_t jp_poller_poll(JP_ATTR_UNUSED jp_poller_t* poller, JP_ATTR_UNUSED int fd) {
     return 0;
 }
 
-jp_errno_t jp_poller_wait(JP_UNUSED jp_poller_t* poller) {
+jp_errno_t jp_poller_wait(JP_ATTR_UNUSED jp_poller_t* poller) {
     return 0;
 }
 
-void jp_poller_destroy(JP_UNUSED jp_poller_t* poller) {
-    return;
+void jp_poller_destroy(JP_ATTR_UNUSED jp_poller_t* poller) {
 }
